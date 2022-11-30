edcfa416c076d948c13092cea36e7edb9b6963a6
Avinash S <sylendra@tcd.ie>
2022-11-30 07:32:55 +0000

    Add initial tweets and get by city

--- twitscrape.py
+++ twitscrape.py
@@ -4,21 +4,33 @@ import itertools
 
 search_terms = ['queen', 'elizabeth', 'colonialism', 'rip', 'royal', 'family', 'death', 'irish',
                 'britain', 'uk']
-exclude_terms = ['mermaid', 'minaj', 'latifah']
+exclude_terms = ['-mermaid', 'minaj', 'latifah']
 tweet_fields = ['date', 'content', 'lang', ]
-locations = {'nagpur': '3000'}
+locations = {'southasia': {'india': ['delhi', 'mumbai', 'chennai']}, 
+             'ireland': {'ireland': ['dublin', 'cork', 'galway']},
+             'uk': {'uk': ['london', 'glasgow', 'york', 'swansea']}}
 
-exlude_query = 'OR -'.join(term for term in exclude_terms)
-print(exclude_terms)
+exclude_query = ' OR -'.join(term for term in exclude_terms)
+
 
 tweets = pd.DataFrame
 
-for location in locations.keys():
-    search_term_query = f"({' OR'.join(term for term in search_terms)}) near:{location} within:{locations[location]} {exclude_terms}"
-    scraped_tweets = sntwit.TwitterSearchScraper(search_term_query).get_items()
-    sliced_scraped_tweets = itertools.islice(scraped_tweets, 1000)
-    print(sliced_scraped_tweets)
-    tweets = pd.concat(tweets, pd.DataFrame(sliced_scraped_tweets))
+for region in locations:
+    for country in locations[region]:
+        city_list = locations[region][country]
+        tweets = None
+        for city in city_list:
+            print(city)
+            search_term_query = f"({' OR '.join(term for term in search_terms)}) near:{city} since:2022-09-08 until:2022-09-10"
+            print(search_term_query)
+            scraped_tweets = sntwit.TwitterSearchScraper(search_term_query).get_items()
+            sliced_scraped_tweets = itertools.islice(scraped_tweets, 1000)
+            results = pd.DataFrame(sliced_scraped_tweets)[['date', 'content']]
+            print(results.head())
+            tweets = pd.concat([tweets, results])
+            # tweets = pd.DataFrame(sliced_scraped_tweets)[['date', 'content']]
+        print(tweets.head())
+        with open(f'tweets_{country}.csv', 'w+', encoding="utf-8") as file:
+            tweets.to_csv(file, sep=',', encoding='utf-8')
+    
 
-with open('tweets.csv', 'w+') as file:
-    tweets.to_csv(file, sep=',')
