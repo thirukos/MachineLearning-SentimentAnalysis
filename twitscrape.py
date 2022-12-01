9ad022bc3bbc19a965f5d1ec0dbf9db3eea033b8
Avinash S <sylendra@tcd.ie>
2022-12-01 16:24:23 +0000

    Add dataset for other locations

--- twitscrape.py
+++ twitscrape.py
@@ -9,21 +9,33 @@ search_terms = ['queen', 'elizabeth', 'colonialism', 'rip', 'royal family', 'dea
                 'britain', 'uk']
 exclude_terms = ['-mermaid', 'minaj', 'latifah']
 tweet_fields = ['date', 'content', 'lang', ]
-locations = {'southasia': {'india': ['delhi', 'mumbai', 'chennai']}, 
-             'ireland': {'ireland': ['dublin', 'cork', 'galway']},
-             'uk': {'uk': ['london', 'glasgow', 'york', 'swansea']}}
+locations = {'southasia': {'india': ['delhi', 'mumbai', 'chennai', 'bangalore', 'kolkata'],
+                           'pakistan':['karachi', 'lahore'],
+                           'nepal':['Kathmandu']}, 
+             'ireland': {'ireland': ['dublin', 'galway']},
+             'uk': {'uk': ['london', 'glasgow', 'york', 'swansea']},
+             'us': {'useast': ['"new york"', 'washington'],
+                    'uswest': ['"san fransisco"', '"los angeles"'],
+                    'uscentral': ['"Kansas City"', 'denver']},
+             'canada': {'canada': ['toronto', 'montreal', 'vancouver']},
+             'europe': {'germany': ['berlin', 'munich'],
+                        'sweden': ['stockholm']}}
 
 exclude_query = ' OR -'.join(term for term in exclude_terms)
 
 
 tweets = pd.DataFrame()
 
-# Using a word cloud to select popular words in timeframe
-search_query = 'since:2022-09-08 until:2022-09-10'
-scraped_tweets = sntwit.TwitterSearchScraper(search_query).get_items()
-wordcloud = WordCloud(stopwords = STOPWORDS, background_color="white").generate(scraped_tweets)
-plt.imshow(wordcloud)
-plt.show()
+# # Using a word cloud to select popular words in timeframe
+# search_query = 'since:2022-09-08 until:2022-09-10'
+# scraped_tweets = sntwit.TwitterSearchScraper(search_query).get_items()
+# sliced_scraped_tweets = itertools.islice(scraped_tweets, 60000)
+# for tweet in scraped_tweets:
+#     tweet = str(tweet)
+#     tokens = tweet.split()
+# wordcloud = WordCloud(stopwords = STOPWORDS, background_color="white").generate(sliced_scraped_tweets)
+# plt.imshow(wordcloud)
+# plt.show()
 
 
 for region in locations:
@@ -32,7 +44,7 @@ for region in locations:
         tweets = None
         for city in city_list:
             print(city)
-            search_term_query = f"({' OR '.join(term for term in search_terms)}) near:{city} since:2022-09-08 until:2022-09-10 lang:en"
+            search_term_query = f'({" OR ".join(term for term in search_terms)}) near:{city} within:10km since:2022-09-08 until:2022-09-10 lang:en'
             print(search_term_query)
             scraped_tweets = sntwit.TwitterSearchScraper(search_term_query).get_items()
             sliced_scraped_tweets = itertools.islice(scraped_tweets, 1000)
